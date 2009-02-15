15bc79416594fdc3b6ea8d7b4ef1722a1137e39f
Aleksey Palazhchenko <alek@silverstone.name>
2009-02-15 21:12:44 +0300

    fixed stupid lock in reporter

--- demo/reporter/main.cpp
+++ demo/reporter/main.cpp
@@ -14,9 +14,8 @@ int main(int argc, char* argv[])
 	BreakpadQt::Sender sender(QUrl(QLatin1String("http://localhost:8080/breakpad-test/receiver")));
 	sender.addParameter(QLatin1String("param1"), QString::fromLatin1("value1"));
 	sender.setFile(qApp->applicationFilePath());
-	sender.send();
-
 	app.connect(&sender, SIGNAL(done(bool)), SLOT(quit()));
+	sender.send();
 	int res = app.exec();
 	sender.wait();
 	return res;
